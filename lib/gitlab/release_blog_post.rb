ac24fa0f7f38dd65a9eae222bf03c446721d8d60
Brandon Labuschagne <blabuschagne@gitlab.com>
2018-12-05 09:37:33 +0200

    Refactored by cleaning up code

--- lib/gitlab/release_blog_post.rb
+++ lib/gitlab/release_blog_post.rb
@@ -7,29 +7,32 @@ module Gitlab
 
     attr_reader :url
 
-    RELEASE_RSS_URL = 'https://about.gitlab.com/releases.xml'.freeze
+    RELEASE_RSS_URL = 'https://about.gitlab.com/releases.xml'
 
-    def fetch_blog_post_url(version)
+    def initialize
+      installed_version = Gitlab.final_release? ? Gitlab.minor_release : Gitlab.previous_release
+      @url = fetch_blog_post_url(installed_version)
+    end
+
+    private
+
+    def fetch_blog_post_url(installed_version)
       response = Gitlab::HTTP.get(RELEASE_RSS_URL, verify: false)
+
       if response.code == 200
         response['feed']['entry'].each do |entry|
-          if entry['release'] == version
-            return entry['id']
-          elsif display_previous_release_post? && entry['release'][/\d+/] == version
-            return entry['id']
-          end
+          return entry['id'] if entry['release'] == installed_version || matches_previous_release_post(entry['release'], installed_version)
         end
         nil
       end
     end
 
-    def initialize
-      version = Gitlab.final_release? ? Gitlab.minor_release : Gitlab.previous_release
-      @url = fetch_blog_post_url(version)
+    def should_match_previous_release_post?
+      Gitlab.new_major_release? && !Gitlab.final_release?
     end
 
-    def display_previous_release_post?
-      Gitlab.new_major_release? && !Gitlab.final_release?
+    def matches_previous_release_post(rss_release_version, installed_version)
+      should_match_previous_release_post? && rss_release_version[/\d+/] == installed_version
     end
   end
 end
