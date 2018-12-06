26d4cf4559c9288915a9e4542593b3326e09e1d6
Brandon Labuschagne <blabuschagne@gitlab.com>
2018-12-06 12:25:58 +0200

    Add boilerplate test code

---
+++ spec/javascripts/set_status_modal/emoji_menu_in_modal.js
@@ -0,0 +1,14 @@
+// import $ from 'jquery';
+// import EmojiMenuInModal from '../../../app/assets/javascripts/set_status_modal/emoji_menu_in_modal';
+
+describe('EmojiMenuInModal', () => {
+  describe('postEmoji', () => {
+    it('updates the status emoji when a new emoji is selected', () => {
+
+    });
+
+    it('calls `selectEmojiCallback` when the emoji is posted', () => {
+
+    });
+  });
+});
