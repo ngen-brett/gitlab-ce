0b81ae50e0bc6d5480310a4e1419b909e8b739fe
Peter Leitzen <pleitzen@gitlab.com>
2018-12-13 17:19:27 +0100

    Show error tracking settings

--- app/models/error_tracking/error_tracking_setting.rb
+++ app/models/error_tracking/error_tracking_setting.rb
@@ -8,5 +8,9 @@ module ErrorTracking
       mode: :per_attribute_iv,
       key: Settings.attr_encrypted_db_key_base_truncated,
       algorithm: 'aes-256-gcm'
+
+    def self.for_project(project)
+      self.where(project: project).first_or_initialize
+    end
   end
 end
