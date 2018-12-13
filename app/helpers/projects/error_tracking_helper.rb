d5bd6c33d1ac99cf5c899df147d73d44bcebdd5e
Simon Knox <psimyn@gmail.com>
2018-12-13 14:28:48 +0100

    Add feature flag view

--- app/helpers/projects/error_tracking_helper.rb
+++ app/helpers/projects/error_tracking_helper.rb
@@ -6,7 +6,8 @@ module Projects::ErrorTrackingHelper
       'index-path' => namespace_project_error_tracking_index_path(
         namespace_id: project.namespace,
         project_id: project
-      )
+      ),
+      'enable-error-tracking-link' => 'https://todo.later'
     }
   end
 end
