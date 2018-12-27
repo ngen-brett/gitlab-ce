09d756a79679068e8370ff45a3a9046f1e583de5
rpereira2 <rpereira@gitlab.com>
2018-12-27 18:18:44 +0530

    Remove the for_project class method
    Since it is not used here. Also change the uri column to api_uri so that
    it is more explicit.

--- app/models/error_tracking/error_tracking_setting.rb
+++ app/models/error_tracking/error_tracking_setting.rb
@@ -8,9 +8,5 @@ module ErrorTracking
       mode: :per_attribute_iv,
       key: Settings.attr_encrypted_db_key_base_truncated,
       algorithm: 'aes-256-gcm'
-
-    def self.for_project(project)
-      self.where(project: project).first_or_initialize
-    end
   end
 end
