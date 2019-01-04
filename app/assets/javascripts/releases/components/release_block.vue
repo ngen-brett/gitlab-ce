bd949ae9c71733e7329218c0898695520b6089c4
jhampton <jhampton@gitlab.com>
2019-01-04 14:45:59 -0500

    Conditionally displays external source - Conditionally displays the (external source) label for external links

--- app/assets/javascripts/releases/components/release_block.vue
+++ app/assets/javascripts/releases/components/release_block.vue
@@ -98,13 +98,7 @@ export default {
           <li v-for="link in assets.links" :key="link.name" class="append-bottom-8">
             <gl-link v-gl-tooltip.bottom :title="__('Download asset')" :href="link.url">
               <icon name="package" class="align-middle append-right-4 align-text-bottom" />
-              // Somethinig like this?
-              // if link.external?
-              //   {{ link.name }} + '(external source)'
-              // else
-              //   {{ link.name }}
-              // end
-              {{ link.name }}
+              {{ link.name }} <span v-if="link.external"> {{ __('(external source)') }}</span>
             </gl-link>
           </li>
         </ul>
