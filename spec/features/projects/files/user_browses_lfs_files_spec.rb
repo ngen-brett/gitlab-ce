b62454b3c18e90e2e93b95b3463a32f68d90a810
Sean McGivern <sean@gitlab.com>
2019-01-08 11:05:59 +0000

    Wait for requests in LFS files spec
    Without this, sometimes we will get a unique constraint violation about
    GPG signatures on MySQL.

--- spec/features/projects/files/user_browses_lfs_files_spec.rb
+++ spec/features/projects/files/user_browses_lfs_files_spec.rb
@@ -12,6 +12,7 @@ describe 'Projects > Files > User browses LFS files' do
     before do
       allow_any_instance_of(Project).to receive(:lfs_enabled?).and_return(false)
       visit project_tree_path(project, 'lfs')
+      wait_for_requests
     end
 
     it 'is possible to see raw content of LFS pointer' do
@@ -30,6 +31,7 @@ describe 'Projects > Files > User browses LFS files' do
     before do
       allow_any_instance_of(Project).to receive(:lfs_enabled?).and_return(true)
       visit project_tree_path(project, 'lfs')
+      wait_for_requests
     end
 
     it 'shows an LFS object' do
