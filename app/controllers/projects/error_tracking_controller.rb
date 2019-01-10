576316f17be6172d7218e3e6ae41e404b7adca8d
Peter Leitzen <pleitzen@gitlab.com>
2019-01-10 18:50:14 +0100

    Remove `error_tracking` feature flag

--- app/controllers/projects/error_tracking_controller.rb
+++ app/controllers/projects/error_tracking_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Projects::ErrorTrackingController < Projects::ApplicationController
-  before_action :check_feature_flag!
   before_action :authorize_read_sentry_issue!
   before_action :push_feature_flag_to_frontend
 
@@ -42,10 +41,6 @@ class Projects::ErrorTrackingController < Projects::ApplicationController
     ErrorTracking::ErrorSerializer
       .new(project: project, user: current_user)
       .represent(errors)
-  end
-
-  def check_feature_flag!
-    render_404 unless Feature.enabled?(:error_tracking, project)
   end
 
   def push_feature_flag_to_frontend
