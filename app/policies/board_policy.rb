a5a9521aac5df32235abe676ae075849638555d4
Mario de la Ossa <mariodelaossa@gmail.com>
2019-01-17 11:40:37 -0600

    Backport of 54385-board-policy

---
+++ app/policies/board_policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class BoardPolicy < BasePolicy
+  delegate { @subject.parent }
+
+  condition(:is_group_board) { @subject.group_board? }
+
+  rule { is_group_board ? can?(:read_group) : can?(:read_project) }.enable :read_parent
+
+  rule { is_group_board & can?(:read_group) }.policy do
+    enable :read_milestone
+  end
+end
