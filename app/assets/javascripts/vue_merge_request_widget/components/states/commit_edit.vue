0734057fff51541fb8d3b28c8bcd54854c8b8db2
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-21 19:40:17 +0200

    Add a toggle for existing commit messages list

--- app/assets/javascripts/vue_merge_request_widget/components/states/commit_edit.vue
+++ app/assets/javascripts/vue_merge_request_widget/components/states/commit_edit.vue
@@ -1,5 +1,9 @@
 <script>
+import Icon from '~/vue_shared/components/icon.vue';
 export default {
+  components: {
+    Icon,
+  },
   props: {
     value: {
       type: String,
@@ -16,23 +20,33 @@ export default {
 
 <template>
   <li>
-    <div v-if="squash"><strong>Commit message</strong></div>
+    <div v-if="squash"><strong>Commit author placeholder</strong></div>
     <div class="prepend-top-default commit-message-editor">
-      <div class="form-group clearfix">
+      <div class="commit-message-label">
         <label class="col-form-label" for="commit-message">
           <strong>Squash commit message</strong>
         </label>
-        <div class="commit-message-container">
-          <textarea
-            id="commit-message"
-            :value="value"
-            @input="$emit('input', $event.target.value)"
-            class="form-control js-commit-message"
-            required="required"
-            rows="14"
-            name="Commit message"
-          ></textarea>
-        </div>
+        <button type="button" class="btn-link btn-blank">
+          Use an existing commit message
+          <icon
+            name="chevron-down"
+            :size="16"
+            aria-hidden="true"
+            class="commits-header-icon"
+            @click.stop="$emit('toggleCommitsList')"
+          />
+        </button>
+      </div>
+      <div class="commit-message-container">
+        <textarea
+          id="commit-message"
+          :value="value"
+          @input="$emit('input', $event.target.value)"
+          class="form-control js-commit-message"
+          required="required"
+          rows="14"
+          name="Commit message"
+        ></textarea>
       </div>
     </div>
   </li>
