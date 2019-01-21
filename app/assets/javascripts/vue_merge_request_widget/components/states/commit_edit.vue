5a80c282c9c9f7f5b56822d35a8e591da2c411b2
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-21 19:00:43 +0200

    Changed commit details structure
    - delete previous component
    - delete previous test
    - create basic component with textarea

---
+++ app/assets/javascripts/vue_merge_request_widget/components/states/commit_edit.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    squash: {
+      type: Boolean,
+      required: false,
+    },
+  },
+};
+</script>
+
+<template>
+  <li>
+    <div v-if="squash">Commit author placeholder</div>
+    <div class="prepend-top-default commit-message-editor">
+      <div class="form-group clearfix">
+        <label class="col-form-label" for="commit-message"> Commit message </label>
+        <div class="commit-message-container">
+          <textarea
+            id="commit-message"
+            :value="value"
+            @input="$emit('input', $event.target.value)"
+            class="form-control js-commit-message"
+            required="required"
+            rows="14"
+            name="Commit message"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
