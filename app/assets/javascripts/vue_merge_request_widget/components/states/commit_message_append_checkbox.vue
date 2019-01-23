f4734a4d32820569fc3a1f2edbf7f0ad636a267c
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-23 11:33:42 +0200

    Abstract checkbox to separate component

---
+++ app/assets/javascripts/vue_merge_request_widget/components/states/commit_message_append_checkbox.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    inputId: {
+      type: String,
+      required: true,
+      default: 'commit-append-checkbox',
+    },
+    append: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleAppend(isAppended) {
+      if (isAppended) {
+        this.$emit('input', value + '\n\r' + append);
+      } else {
+        this.$emit('input', value.replace(`\n\r\${append}`, ''));
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <label>
+    <input :id="inputId" type="checkbox" @change="handleAppend($event.target.checked)" />
+    {{ label }}
+  </label>
+</template>
