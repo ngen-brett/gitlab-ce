e77c66cd1dd0f905e9f3e5f92506ad54bae94182
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-24 13:58:16 +0200

    Removed unused method and extended tests
    - added tests for textarea

--- spec/javascripts/vue_mr_widget/components/states/mr_widget_commit_edit_spec.js
+++ spec/javascripts/vue_mr_widget/components/states/mr_widget_commit_edit_spec.js
@@ -6,21 +6,51 @@ const localVue = createLocalVue();
 describe('Commits edit component', () => {
   let wrapper;
 
-  const createComponent = props => {
+  const createComponent = () => {
     wrapper = shallowMount(localVue.extend(CommitEdit), {
       localVue,
       sync: false,
       propsData: {
-        isSquashEnabled: false,
-        targetBranch: 'master',
-        expanded: false,
-        commitsCount: 5,
-        ...props,
+        value: 'Test commit message',
+        label: 'Test label',
+        inputId: 'test-input-id',
       },
     });
   };
 
+  beforeEach(() => {
+    createComponent();
+  });
+
   afterEach(() => {
     wrapper.destroy();
+  });
+
+  const findTextarea = () => wrapper.find('.js-commit-message');
+
+  it('has a correct label', () => {
+    const labelElement = wrapper.find('.col-form-label');
+
+    expect(labelElement.text()).toBe('Test label');
+  });
+
+  describe('textarea', () => {
+    it('has a correct ID', () => {
+      expect(findTextarea().attributes('id')).toBe('test-input-id');
+    });
+
+    it('has a correct value', () => {
+      expect(findTextarea().element.value).toBe('Test commit message');
+    });
+
+    it('emits an input event and receives changed value', () => {
+      const changedCommitMessage = 'Changed commit message';
+
+      findTextarea().element.value = changedCommitMessage;
+      findTextarea().trigger('input');
+
+      expect(wrapper.emitted().input[0]).toEqual([changedCommitMessage]);
+      expect(findTextarea().element.value).toBe(changedCommitMessage);
+    });
   });
 });
