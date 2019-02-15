044a41d2e18200c4bc2627435c4018e7f5a44c6b
rpereira2 <rpereira@gitlab.com>
2019-02-15 18:16:15 +0530

    Add feature spec to check error tracking form

--- spec/features/projects/settings/operations_settings_spec.rb
+++ spec/features/projects/settings/operations_settings_spec.rb
@@ -22,12 +22,46 @@ describe 'Projects > Settings > For a forked project', :js do
   end
 
   describe 'Settings > Operations' do
-    it 'renders the Error Tracking settings' do
+    let(:sentry_list_projects_url) { 'http://sentry.example.com/api/0/projects/' }
+
+    let(:projects_sample_response) do
+      Gitlab::Utils.deep_indifferent_access(
+        JSON.parse(fixture_file('sentry/list_projects_sample_response.json'))
+      )
+    end
+
+    before do
+      WebMock.stub_request(:get, sentry_list_projects_url)
+      .to_return(
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: projects_sample_response.to_json
+      )
+    end
+
+    it 'fills and submits the Error Tracking settings form' do
       visit project_settings_operations_path(project)
 
       wait_for_requests
 
       expect(page).to have_content('Sentry API URL')
+      expect(page.body).to include('Error Tracking')
+      expect(page).to have_button('Connect')
+
+      check('Active')
+      fill_in('error-tracking-api-host', with: 'http://sentry.example.com')
+      fill_in('error-tracking-token', with: 'token')
+
+      click_button('Connect')
+
+      within(:xpath, '//div[@id="project-dropdown"]') do
+        click_button('Select project')
+        click_button('Sentry | Internal')
+      end
+
+      click_button('Save changes')
+
+      assert_text('Your changes have been saved.')
     end
   end
 end
