835d32f296de395e55df7f39a777bebdad310c38
Alexandru Croitor <acroitor@gitlab.com>
2019-02-21 11:35:01 +0200

    Add email_header_and_footer_enabled toggle in admin appearance UI

--- spec/controllers/admin/appearances_controller_spec.rb
+++ spec/controllers/admin/appearances_controller_spec.rb
@@ -24,8 +24,33 @@ describe Admin::AppearancesController do
 
       expect(Appearance.current).to have_attributes(
         header_message: header_message,
-        footer_message: footer_message
+        footer_message: footer_message,
+        email_header_and_footer_enabled: false,
+        message_background_color: '#E75E40',
+        message_font_color: '#FFFFFF'
       )
+    end
+
+    context "when enabling header and footer in email" do
+      let(:create_params) do
+        {
+          title: "Foo",
+          description: "Bar",
+          header_message: header_message,
+          footer_message: footer_message,
+          email_header_and_footer_enabled: true,
+        }
+      end
+
+      it 'creates appearance with enabled flag' do
+        post :create, params: { appearance: create_params }
+
+        expect(Appearance.current).to have_attributes(
+          header_message: header_message,
+          footer_message: footer_message,
+          email_header_and_footer_enabled: true,
+        )
+      end
     end
   end
 
@@ -48,8 +73,34 @@ describe Admin::AppearancesController do
 
       expect(Appearance.current).to have_attributes(
         header_message: header_message,
-        footer_message: footer_message
+        footer_message: footer_message,
+        email_header_and_footer_enabled: false,
+        message_background_color: '#E75E40',
+        message_font_color: '#FFFFFF'
       )
+    end
+
+
+    context "when enabling header and footer in email" do
+      let(:create_params) do
+        {
+          title: "Foo",
+          description: "Bar",
+          header_message: header_message,
+          footer_message: footer_message,
+          email_header_and_footer_enabled: true,
+        }
+      end
+
+      it 'updates appearance with enabled flag' do
+        post :update, params: { appearance: create_params }
+
+        expect(Appearance.current).to have_attributes(
+          header_message: header_message,
+          footer_message: footer_message,
+          email_header_and_footer_enabled: true,
+        )
+      end
     end
   end
 end
