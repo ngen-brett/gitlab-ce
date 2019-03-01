ee5132b38c4ffc049ca75af5a64597790069d982
Paul Slaughter <pslaughter@gitlab.com>
2019-03-01 14:29:46 -0600

    Remove files_decorator_worker with files_decorator
    **Why?**
    This operation takes only a few hundred milliseconds.
    Using web workers is actually a performance hit since it has
    to copy the huge object from one thread to another.

--- app/assets/javascripts/ide/lib/files_decorator_worker.js
+++
@@ -1,9 +0,0 @@
-import { decorateFiles } from './files_decorator';
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('message', e => {
-  const result = decorateFiles(e.data);
-
-  // eslint-disable-next-line no-restricted-globals
-  self.postMessage(result);
-});
