a9722a556d78bd3b181dffcc83e75dc09cc15ff8
mfluharty <mfluharty@gitlab.com>
2019-03-13 17:35:23 -0600

    Add pipeline triggerer column, adjust columns
    Move pipeline triggerer avatar to its own column
    Adjust the widths of other columns to accommodate new column
    Add wrap class to commit column to prevent overlap

--- app/assets/javascripts/pipelines/components/pipelines_table.vue
+++ app/assets/javascripts/pipelines/components/pipelines_table.vue
@@ -65,13 +65,16 @@ export default {
       <div class="table-section section-10 js-pipeline-status pipeline-status" role="rowheader">
         {{ s__('Pipeline|Status') }}
       </div>
-      <div class="table-section section-15 js-pipeline-info pipeline-info" role="rowheader">
+      <div class="table-section section-10 js-pipeline-info pipeline-info" role="rowheader">
         {{ s__('Pipeline|Pipeline') }}
+      </div>
+      <div class="table-section section-10 js-triggerer-info triggerer-info" role="rowheader">
+        {{ s__('Pipeline|Triggerer') }}
       </div>
       <div class="table-section section-20 js-pipeline-commit pipeline-commit" role="rowheader">
         {{ s__('Pipeline|Commit') }}
       </div>
-      <div class="table-section section-20 js-pipeline-stages pipeline-stages" role="rowheader">
+      <div class="table-section section-15 js-pipeline-stages pipeline-stages" role="rowheader">
         {{ s__('Pipeline|Stages') }}
       </div>
     </div>
