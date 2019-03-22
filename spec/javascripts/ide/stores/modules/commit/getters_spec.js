e9f7bdc6944b5fc50157027dee0eb966c434f6a4
Natalia Tepluhina <tarya.se@gmail.com>
2019-03-22 16:20:55 +0200

    Revert "Renamed test spec"
    This reverts commit 225a88379d210d2750ce500d69fd551515c686ed.

--- spec/javascripts/ide/stores/modules/commit/getters_spec.js
+++ spec/javascripts/ide/stores/modules/commit/getters_spec.js
@@ -55,7 +55,7 @@ describe('IDE commit module getters', () => {
       });
     });
 
-    it('defaults to currentBranchId', () => {
+    it('defualts to currentBranchId', () => {
       expect(getters.branchName(state, null, rootState)).toBe('master');
     });
 
@@ -71,7 +71,7 @@ describe('IDE commit module getters', () => {
           expect(getters.branchName(state, localGetters, rootState)).toBe('state-newBranchName');
         });
 
-        it('uses placeholderBranchName when state newBranchName is empty', () => {
+        it('uses getters newBranchName when state newBranchName is empty', () => {
           Object.assign(state, {
             newBranchName: '',
           });
