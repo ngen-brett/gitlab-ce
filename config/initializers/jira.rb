5ae5f6fa9e133e6bd9acf9a9e3be1028772183a4
Felipe Artur <felipefac@gmail.com>
2019-03-27 12:39:55 -0300

    Load 'Gitlab::Jira::Middleware' if it exists
    Loads 'Gitlab::Jira::Middleware' only if it exists
    so this initializer can be backported to CE.

---
+++ config/initializers/jira.rb
@@ -0,0 +1,9 @@
+# Treats JIRA DVCS user agent requests in order to be successfully handled
+# by our API.
+#
+# Gitlab::Jira::Middleware is only defined on EE
+#
+# Use safe_constantize because the class may exist but still not be loaded
+if "Gitlab::Jira::Middleware".safe_constantize
+  Rails.application.config.middleware.use(Gitlab::Jira::Middleware)
+end
