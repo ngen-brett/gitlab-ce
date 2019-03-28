11a2db125eb51f8f1cf910278aee024634637550
Felipe Artur <fcardozo@gitlab.com>
2019-03-28 13:02:52 +0000

    Update jira.rb

--- config/initializers/jira.rb
+++ config/initializers/jira.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# Treats JIRA DVCS user agent requests in order to be successfully handled
+# Changes JIRA DVCS user agent requests in order to be successfully handled
 # by our API.
 #
 # Gitlab::Jira::Middleware is only defined on EE
 #
-# Use safe_constantize because the class may exist but still not be loaded
+# Use safe_constantize because the class may exist but has not been loaded yet
 if "Gitlab::Jira::Middleware".safe_constantize
   Rails.application.config.middleware.use(Gitlab::Jira::Middleware)
 end
