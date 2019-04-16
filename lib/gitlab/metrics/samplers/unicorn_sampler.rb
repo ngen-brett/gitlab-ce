0e84362e3c37b5973a851f2dbcc573a3c7f65cfd
Ryan Cobb <ryan.david.cobb@gmail.com>
2019-04-16 13:26:18 -0600

    Test new unicorn metrics

--- lib/gitlab/metrics/samplers/unicorn_sampler.rb
+++ lib/gitlab/metrics/samplers/unicorn_sampler.rb
@@ -16,6 +16,22 @@ module Gitlab
           @unicorn_queued_connections ||= ::Gitlab::Metrics.gauge(:unicorn_queued_connections, 'Unicorn queued connections', {}, :max)
         end
 
+        def process_cpu_seconds_total_gauge
+          @process_cpu_seconds_total ||= ::Gitlab::Metrics.gauge(:process_cpu_seconds_total, 'Process CPU seconds total', {}, :livesum)
+        end
+
+        def process_start_time_seconds_gauge
+          @process_start_time_seconds ||= ::Gitlab::Metrics.gauge(:process_start_time_seconds, 'Process start time seconds', {}, :max)
+        end
+
+        def process_max_fds_gauge
+          @process_max_fds ||= ::Gitlab::Metrics.gauge(:process_max_fds, 'Process max fds', {}, :max)
+        end
+
+        def unicorn_workers_gauge
+          @unicorn_workers ||= ::Gitlab::Metrics.gauge(:unicorn_workers, 'Unicorn workers', {}, :max)
+        end
+
         def enabled?
           # Raindrops::Linux.tcp_listener_stats is only present on Linux
           unicorn_with_listeners? && Raindrops::Linux.respond_to?(:tcp_listener_stats)
@@ -31,12 +47,34 @@ module Gitlab
             unicorn_active_connections.set({ socket_type: 'unix', socket_address: addr }, stats.active)
             unicorn_queued_connections.set({ socket_type: 'unix', socket_address: addr }, stats.queued)
           end
+
+          ps = Sys::ProcTable.ps(pid: pid)
+          process_cpu_seconds_total_gauge.set({ worker: pid }, process_cpu_seconds_total(ps))
+          process_start_time_seconds_gauge.set({ worker: pid }, process_start_time_seconds(ps))
+          process_max_fds_gauge.set({ worker: pid }, process_max_fds)
+          unicorn_workers_gauge.set({}, unix_listeners.count)
         end
 
         private
 
         def tcp_listeners
           @tcp_listeners ||= Unicorn.listener_names.grep(%r{\A[^/]+:\d+\z})
+        end
+
+        def pid
+          @pid ||= Process.pid
+        end
+
+        def process_cpu_seconds_total(ps)
+          (ps.stime + ps.utime) / 100
+        end
+
+        def process_start_time_seconds(ps)
+          @process_start_time_seconds ||= ps.starttime / 100
+        end
+
+        def process_max_fds
+          @process_max_fds_gauge ||= File.open('/proc/self/limits').match(/Max open files\s*(\d+)/)
         end
 
         def unix_listeners
