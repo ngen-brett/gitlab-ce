19c87cb8b4218ed7113237d5ac3c892895cb39b6
Alessio Caiazza <acaiazza@gitlab.com>
2019-04-16 12:10:35 +0200

    Extract ProjectStatistics updates into a concern
    Refactor existing tests as a shared example

---
+++ app/models/concerns/update_project_statistics.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module UpdateProjectStatistics
+  extend ActiveSupport::Concern
+
+  class_methods do
+    attr_reader :update_project_statistics_attribute,
+                :update_project_statistics_stat
+
+    private
+
+    def update_project_statistics(stat:, attribute: :size)
+      @update_project_statistics_stat = stat
+      @update_project_statistics_attribute = attribute
+
+      after_save(:update_project_statistics_after_save, if: :update_project_statistics_attribute_changed?)
+      after_destroy(:update_project_statistics_after_destroy, unless: :project_destroyed?)
+    end
+  end
+
+  included do
+    def project_destroyed?
+      project.pending_delete?
+    end
+
+    def update_project_statistics_attribute_changed?
+      attribute_changed?(self.class.update_project_statistics_attribute)
+    end
+
+    def update_project_statistics_after_save
+      attr = self.class.update_project_statistics_attribute
+      delta = read_attribute(attr).to_i - attribute_was(attr).to_i
+
+      update_project_statistics(delta)
+    end
+
+    def update_project_statistics_after_destroy
+      update_project_statistics(-read_attribute(self.class.update_project_statistics_attribute).to_i)
+    end
+
+    def update_project_statistics(delta)
+      ProjectStatistics.increment_statistic(project_id, self.class.update_project_statistics_stat, delta)
+    end
+  end
+end
