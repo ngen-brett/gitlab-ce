2c40c141214387665b297426315d65c408f2c456
Ryan Cobb <ryan.david.cobb@gmail.com>
2019-04-17 15:47:32 -0600

    testing

--- lib/gitlab/metrics/samplers/unicorn_sampler.rb
+++ lib/gitlab/metrics/samplers/unicorn_sampler.rb
@@ -8,28 +8,21 @@ module Gitlab
           super(interval)
         end
 
-        def unicorn_active_connections
-          @unicorn_active_connections ||= ::Gitlab::Metrics.gauge(:unicorn_active_connections, 'Unicorn active connections', {}, :max)
+        def metrics
+          @metrics ||= init_metrics
         end
 
-        def unicorn_queued_connections
-          @unicorn_queued_connections ||= ::Gitlab::Metrics.gauge(:unicorn_queued_connections, 'Unicorn queued connections', {}, :max)
-        end
+        def init_metrics
+          ps = Sys::ProcTable.ps(pid: pid)
 
-        def process_cpu_seconds_total_gauge
-          @process_cpu_seconds_total ||= ::Gitlab::Metrics.gauge(:process_cpu_seconds_total, 'Process CPU seconds total', {})
-        end
-
-        def process_start_time_seconds_gauge
-          @process_start_time_seconds ||= ::Gitlab::Metrics.gauge(:process_start_time_seconds, 'Process start time seconds', {})
-        end
-
-        def process_max_fds_gauge
-          @process_max_fds ||= ::Gitlab::Metrics.gauge(:process_max_fds, 'Process max fds', {})
-        end
-
-        def unicorn_workers_count
-          @unicorn_workers ||= ::Gitlab::Metrics.count(:unicorn_workers, 'Unicorn workers', {})
+          {
+            unicorn_active_connections: ::Gitlab::Metrics.gauge(:unicorn_active_connections, 'Unicorn active connections', {}, :max),
+            unicorn_queued_connections: ::Gitlab::Metrics.gauge(:unicorn_queued_connections, 'Unicorn queued connections', {}, :max),
+            unicorn_workers_count:      ::Gitlab::Metrics.count(:unicorn_workers, 'Unicorn workers', {}),
+            process_cpu_seconds_total:  ::Gitlab::Metrics.gauge(:process_cpu_seconds_total, 'Process CPU seconds total', {}),
+            process_start_time_seconds: ::Gitlab::Metrics.gauge(:process_start_time_seconds, 'Process start time seconds', {}).set(process_start_time_seconds(ps)),
+            process_max_fds:            ::Gitlab::Metrics.gauge(:process_max_fds, 'Process max fds', {})
+          }
         end
 
         def enabled?
@@ -39,20 +32,19 @@ module Gitlab
 
         def sample
           Raindrops::Linux.tcp_listener_stats(tcp_listeners).each do |addr, stats|
-            unicorn_active_connections.set({ socket_type: 'tcp', socket_address: addr }, stats.active)
-            unicorn_queued_connections.set({ socket_type: 'tcp', socket_address: addr }, stats.queued)
+            metrics[:unicorn_active_connections].set({ socket_type: 'tcp', socket_address: addr }, stats.active)
+            metrics[:unicorn_queued_connections].set({ socket_type: 'tcp', socket_address: addr }, stats.queued)
           end
 
           Raindrops::Linux.unix_listener_stats(unix_listeners).each do |addr, stats|
-            unicorn_active_connections.set({ socket_type: 'unix', socket_address: addr }, stats.active)
-            unicorn_queued_connections.set({ socket_type: 'unix', socket_address: addr }, stats.queued)
+            metrics[:unicorn_active_connections].set({ socket_type: 'unix', socket_address: addr }, stats.active)
+            metrics[:unicorn_queued_connections].set({ socket_type: 'unix', socket_address: addr }, stats.queued)
           end
 
           ps = Sys::ProcTable.ps(pid: pid)
-          process_cpu_seconds_total_gauge.set({ worker: pid }, process_cpu_seconds_total(ps))
-          process_start_time_seconds_gauge.set({ worker: pid }, process_start_time_seconds(ps))
-          process_max_fds_gauge.set({ worker: pid }, process_max_fds)
-          unicorn_workers_count.set({}, unix_listeners.count)
+          metrics[:process_cpu_seconds_total].set({ worker: pid }, process_cpu_seconds_total(ps))
+          # metrics[:process_max_fds].set({ worker: pid }, process_max_fds)
+          metrics[:unicorn_workers_count].set({}, unix_listeners.count)
         end
 
         private
