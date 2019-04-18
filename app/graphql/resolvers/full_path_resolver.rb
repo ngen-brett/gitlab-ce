0e83fe374a8ea551a174aaa52556ff95824fb98b
Brett Walker <bwalker@gitlab.com>
2019-04-18 18:30:34 -0500

    Add group query specs

--- app/graphql/resolvers/full_path_resolver.rb
+++ app/graphql/resolvers/full_path_resolver.rb
@@ -13,8 +13,8 @@ module Resolvers
     def model_by_full_path(model, full_path)
       BatchLoader.for(full_path).batch(key: model) do |full_paths, loader, args|
         # `with_route` avoids an N+1 calculating full_path
-        args[:key].where_full_path_in(full_paths).with_route.each do |project|
-          loader.call(project.full_path, project)
+        args[:key].where_full_path_in(full_paths).with_route.each do |model_instance|
+          loader.call(model_instance.full_path, model_instance)
         end
       end
     end
--- app/graphql/types/group_type.rb
+++ app/graphql/types/group_type.rb
@@ -9,24 +9,27 @@ module Types
     expose_permissions Types::PermissionTypes::Group
 
     field :id, GraphQL::ID_TYPE, null: false
+    field :web_url, GraphQL::STRING_TYPE, null: true
+    field :name, GraphQL::STRING_TYPE, null: false
 
     field :path, GraphQL::STRING_TYPE, null: false
-    field :name, GraphQL::STRING_TYPE, null: false
-    field :full_path, GraphQL::ID_TYPE, null: false
-    field :full_name, GraphQL::STRING_TYPE, null: false
-
     field :description, GraphQL::STRING_TYPE, null: true
-
-    field :web_url, GraphQL::STRING_TYPE, null: true
-    field :file_template_project_id, GraphQL::ID_TYPE, null: true
-    field :parent_id, GraphQL::ID_TYPE, null: true
-    field :shared_runners_minutes_limit, GraphQL::INT_TYPE, null: true
-    field :extra_shared_runners_minutes_limit, GraphQL::INT_TYPE, null: true
     field :visibility, GraphQL::STRING_TYPE, null: true
-    field :request_access_enabled, GraphQL::BOOLEAN_TYPE, null: true
+
+    field :lfs_enabled, GraphQL::BOOLEAN_TYPE, null: true, resolve: -> (group, args, ctx) do
+      group.lfs_enabled?
+    end
 
     field :avatar_url, GraphQL::STRING_TYPE, null: true, resolve: -> (group, args, ctx) do
       group.avatar_url(only_path: false)
+    end
+
+    field :request_access_enabled, GraphQL::BOOLEAN_TYPE, null: true
+    field :full_path, GraphQL::ID_TYPE, null: false
+    field :full_name, GraphQL::STRING_TYPE, null: false
+
+    if ::Group.supports_nested_objects?
+      field :parent_id, GraphQL::ID_TYPE, null: true
     end
   end
 end
