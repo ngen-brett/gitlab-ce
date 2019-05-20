8a4dec3750f9d26e8a24a7aae12cf24ad73c822c
Małgorzata Ksionek <meksionek@gmail.com>
2019-05-20 11:43:06 +0200

    Add timezone_name field
    Add saving timezone_name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190506135400) do
+ActiveRecord::Schema.define(version: 20190519094008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -2258,6 +2258,7 @@ ActiveRecord::Schema.define(version: 20190506135400) do
     t.string "timezone"
     t.boolean "time_display_relative"
     t.boolean "time_format_in_24h"
+    t.string "timezone_name"
     t.index ["user_id"], name: "index_user_preferences_on_user_id", unique: true, using: :btree
   end
 
