3131e5fc84c020fc54f3c045f6fcb5098ee775a2
Fabio Pitino <fpitino@gitlab.com>
2019-06-07 11:37:55 +0100

    Move project default git depth behind feature flags

--- app/models/project_ci_cd_setting.rb
+++ app/models/project_ci_cd_setting.rb
@@ -31,6 +31,7 @@ class ProjectCiCdSetting < ApplicationRecord
   private
 
   def set_default_git_depth
+    return unless Feature.enabled?(:ci_set_project_default_git_depth, default_enabled: true)
     self.default_git_depth ||= DEFAULT_GIT_DEPTH
   end
 end
