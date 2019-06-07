1a073b833b91b24485dd39f4692f82e4effb8b9b
Paul Slaughter <pslaughter@gitlab.com>
2019-06-06 20:50:08 -0500

    Extract ide_status_list from ide_status_bar
    **Why?**
    The ide_status_list will be used and extended in EE.

---
+++ app/assets/javascripts/ide/components/ide_status_list.vue
@@ -0,0 +1,25 @@
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      file: 'activeFile',
+    }),
+  },
+};
+</script>
+
+<template>
+  <div class="ide-status-list d-flex">
+    <template v-if="file">
+      <div class="ide-status-file">{{ file.name }}</div>
+      <div class="ide-status-file">{{ file.eol }}</div>
+      <div v-if="!file.binary" class="ide-status-file">
+        {{ file.editorRow }}:{{ file.editorColumn }}
+      </div>
+      <div class="ide-status-file">{{ file.fileLanguage }}</div>
+    </template>
+    <slot></slot>
+  </div>
+</template>
