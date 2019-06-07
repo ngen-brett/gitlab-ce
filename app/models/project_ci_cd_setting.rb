11180ba34055e8460032db935614d5ab1c9157ad
Fabio Pitino <fpitino@gitlab.com>
2019-06-07 14:51:09 +0100

    Add empty line after guard clause

--- app/models/project_ci_cd_setting.rb
+++ app/models/project_ci_cd_setting.rb
@@ -32,6 +32,7 @@ class ProjectCiCdSetting < ApplicationRecord
 
   def set_default_git_depth
     return unless Feature.enabled?(:ci_set_project_default_git_depth, default_enabled: true)
+
     self.default_git_depth ||= DEFAULT_GIT_DEPTH
   end
 end
