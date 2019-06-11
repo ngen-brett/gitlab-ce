8fe7d2887c40ca9458d89b50827377b8289629c8
Shinya Maeda <shinya@gitlab.com>
2019-06-11 16:34:02 +0700

    Persist target project id and target branch in merge trains table
    In order to optimize the query performance, we should use target
    project id and target branch intead of cross-joining  merge_requests
    table.
    
    Fix schema
    
    Fix schema

---
+++ db/migrate/20190611161641_add_target_project_id_to_merge_trains.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddTargetProjectIdToMergeTrains < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def change
+    add_reference :merge_trains, :target_project, null: false, index: true, foreign_key: { on_delete: :cascade, to_table: :projects }, type: :integer
+    add_column :merge_trains, :target_branch, :text, null: false
+  end
+end
