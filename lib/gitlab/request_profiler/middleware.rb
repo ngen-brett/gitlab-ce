9ed8e2960318bf3a5e4b586aa2cefd55625136da
Qingyu Zhao <qzhao@gitlab.com>
2019-06-27 23:42:14 +1000

    Add profile mode to extend request profiling

--- lib/gitlab/request_profiler/middleware.rb
+++ lib/gitlab/request_profiler/middleware.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'ruby-prof'
+require 'memory_profiler'
 
 module Gitlab
   module RequestProfiler
@@ -10,14 +11,16 @@ module Gitlab
       end
 
       def call(env)
-        if profile?(env)
-          call_with_profiling(env)
+        if profile_execution?(env)
+          call_with_profiling_execution(env)
+        elsif profile_memory?(env)
+          call_with_profiling_memory(env)
         else
           @app.call(env)
         end
       end
 
-      def profile?(env)
+      def valid_profile_token?(env)
         header_token = env['HTTP_X_PROFILE_TOKEN']
         return unless header_token.present?
 
@@ -27,7 +30,25 @@ module Gitlab
         header_token == profile_token
       end
 
-      def call_with_profiling(env)
+      def profile_execution?(env)
+        return unless valid_profile_token?(env)
+
+        profile_mode = env['HTTP_X_PROFILE_MODE']
+        return unless profile_mode.present?
+
+        profile_mode == 'execution'
+      end
+
+      def profile_memory?(env)
+        return unless valid_profile_token?(env)
+
+        profile_mode = env['HTTP_X_PROFILE_MODE']
+        return unless profile_mode.present?
+
+        profile_mode == 'memory'
+      end
+
+      def call_with_profiling_execution(env)
         ret = nil
         result = RubyProf::Profile.profile do
           ret = catch(:warden) do
@@ -43,6 +64,41 @@ module Gitlab
         File.open(file_path, 'wb') do |file|
           printer.print(file)
         end
+
+        if ret.is_a?(Array)
+          ret
+        else
+          throw(:warden, ret)
+        end
+      end
+
+      def prepend_pre_to_file_content(file_path)
+        new_file = "#{file_path}_new_temp"
+        File.open(new_file, 'w') do |fo|
+          fo.puts '<pre>'
+          File.foreach(file_path) do |li|
+            fo.puts li
+          end
+        end
+        File.rename(new_file, file_path)
+      end
+
+      def call_with_profiling_memory(env)
+        ret = nil
+        report = MemoryProfiler.report do
+          ret = catch(:warden) do
+            @app.call(env)
+          end
+        end
+
+        file_name = "#{env['PATH_INFO'].tr('/', '|')}_#{Time.current.to_i}.html"
+        file_path = "#{PROFILES_DIR}/#{file_name}"
+
+        FileUtils.mkdir_p(PROFILES_DIR)
+
+        report.pretty_print(to_file: file_path)
+
+        prepend_pre_to_file_content(file_path)
 
         if ret.is_a?(Array)
           ret
