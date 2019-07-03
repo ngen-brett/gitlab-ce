af6ccda100cf2d87132e245d86c74b5446ddcc0f
Mayra Cabrera <mcabrera@gitlab.com>
2019-07-03 15:14:29 -0500

    Schedule namespace aggregation in other contexts
    Schedules a Namespace::AggregationSchedule worker if some of the project
    statistics are refreshed.
    
    The worker is only executed if the feature flag is enabled.

--- spec/models/project_statistics_spec.rb
+++ spec/models/project_statistics_spec.rb
@@ -135,6 +135,43 @@ describe ProjectStatistics do
         expect(statistics.wiki_size).to eq(0)
       end
     end
+
+    context 'when the column is namespace relatable' do
+      let(:namespace) { create(:group) }
+
+      before do
+        project.update(namespace: namespace)
+      end
+
+      context 'when the feature flag is off' do
+        it 'does not schedule the aggregation worker' do
+          stub_feature_flags(update_statistics_namespace: false, namespace: namespace)
+
+          expect(Namespaces::ScheduleAggregationWorker)
+            .not_to receive(:perform_async)
+
+          statistics.refresh!(only: [:commit_count])
+        end
+      end
+
+      context 'when the feature flag is on' do
+        it 'schedules the aggregation worker' do
+          expect(Namespaces::ScheduleAggregationWorker)
+            .to receive(:perform_async)
+
+          statistics.refresh!(only: [:commit_count])
+        end
+      end
+    end
+
+    context 'when the column is not namespace relatable' do
+      it 'does not schedules an aggregation worker' do
+        expect(Namespaces::ScheduleAggregationWorker)
+          .not_to receive(:perform_async)
+
+        statistics.refresh!(only: [:lfs_objects_size])
+      end
+    end
   end
 
   describe '#update_commit_count' do
