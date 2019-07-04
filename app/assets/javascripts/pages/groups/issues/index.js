a17ee71ea7dd0ef8a80f10554b249035a9088e3f
Fatih Acet <acetfatih@gmail.com>
2019-07-04 15:35:37 +0200

    CE Port of Allow bulk update for group issues
    This is a port MR for CE
    
    Original MR:
    
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/14141

--- app/assets/javascripts/pages/groups/issues/index.js
+++ app/assets/javascripts/pages/groups/issues/index.js
@@ -1,11 +1,15 @@
 import projectSelect from '~/project_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
+import issuableInitBulkUpdateSidebar from 'ee/issuable_init_bulk_update_sidebar';
 import { FILTERED_SEARCH } from '~/pages/constants';
 import IssuableFilteredSearchTokenKeys from 'ee_else_ce/filtered_search/issuable_filtered_search_token_keys';
 import initManualOrdering from '~/manual_ordering';
 
 document.addEventListener('DOMContentLoaded', () => {
   IssuableFilteredSearchTokenKeys.addExtraTokensForIssues();
+
+  // FIXME: Hardcoded prefix
+  issuableInitBulkUpdateSidebar.init('issue_');
 
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
