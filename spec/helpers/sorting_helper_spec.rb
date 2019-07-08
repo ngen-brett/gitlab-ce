31da3c6d11168ce595962798fce00d5806531daf
Ezekiel Kigbo <ekigbo@gitlab.com>
2019-07-08 16:33:42 +1000

    Added test for admin/projects
    Added tests with project_list_filter_bar set to off

--- spec/helpers/sorting_helper_spec.rb
+++ spec/helpers/sorting_helper_spec.rb
@@ -52,26 +52,35 @@ describe SortingHelper do
     allow(helper.controller).to receive(:controller_path).and_return(value)
   end
 
-  def project_common_options()
+  def project_common_options
     {
       sort_value_latest_activity  => sort_title_latest_activity,
       sort_value_recently_created => sort_title_created_date,
       sort_value_name             => sort_title_name,
       sort_value_stars_desc       => sort_title_stars
-    }  
-  end
-
-  def admin_additional_project_options()
-    {
-      sort_value_recently_created => sort_title_recently_created,
-      sort_value_largest_repo     => sort_title_largest_repo,
-      sort_value_oldest_activity  => sort_title_oldest_activity,
-      sort_value_oldest_created   => sort_title_oldest_created,
-      sort_value_stars_desc       => sort_title_most_stars
     }
   end
 
-  describe 'with `admin/projects` controller', :focus do 
+  def admin_additional_project_options
+    {
+      sort_value_oldest_activity  => sort_title_oldest_activity,
+      sort_value_oldest_created   => sort_title_oldest_created,
+      sort_value_recently_created => sort_title_recently_created,
+      sort_value_stars_desc       => sort_title_most_stars,
+      sort_value_largest_repo     => sort_title_largest_repo
+    }
+  end
+
+  def old_project_options
+    project_common_options.merge({
+      sort_value_oldest_activity  => sort_title_oldest_activity,
+      sort_value_oldest_created   => sort_title_oldest_created,
+      sort_value_recently_created => sort_title_recently_created,
+      sort_value_stars_desc       => sort_title_most_stars
+    })
+  end
+
+  describe 'with `admin/projects` controller', :focus do
     before do
       stub_controller_path('admin/projects')
     end
@@ -84,9 +93,22 @@ describe SortingHelper do
         expect(hash).to eq(admin_options)
       end
     end
+
+    describe 'with project_list_filter_bar off' do
+      before do
+        stub_feature_flags(project_list_filter_bar: false)
+      end
+
+      it 'returns a hash of available sorting options for the admin page' do
+        hash = projects_sort_options_hash
+
+        admin_options = project_common_options.merge(admin_additional_project_options)
+        expect(hash).to eq(admin_options)
+      end
+    end
   end
 
-  describe 'with `projects` controller', :focus do
+  describe 'with `projects` controller' do
     before do
       stub_controller_path('projects')
     end
@@ -94,22 +116,11 @@ describe SortingHelper do
     describe '#projects_sort_options_hash' do
       it 'returns a hash of available sorting options' do
         hash = projects_sort_options_hash
-
         common_options = project_common_options
-        admin_options = admin_additional_project_options
-        common_with_different_values = [sort_value_recently_created, sort_value_stars_desc]
 
         common_options.each do |key, opt|
           expect(hash).to include(key)
           expect(hash[key]).to eq(opt)
-        end
-
-        admin_options.each do |key, opt|
-          if common_with_different_values.include?(key)
-            expect(hash[key]).not_to eq(opt)
-          else
-            expect(hash).not_to include(key)
-          end
         end
       end
     end
@@ -135,27 +146,89 @@ describe SortingHelper do
         end
       end
     end
-  end
 
-  describe '#project_sort_direction_button', :focus do
-    before do
-      allow(self).to receive(:request).and_return(double(path: 'http://test.com', query_parameters: { label_name: 'test_label' }))
+    describe '#project_sort_direction_button' do
+      before do
+        allow(self).to receive(:request).and_return(double(path: 'http://test.com', query_parameters: { label_name: 'test_label' }))
+      end
+
+      it 'returns icon with sort-highest when sort is created_date' do
+        expect(project_sort_direction_button('created_date')).to include('sort-highest')
+      end
+
+      it 'returns icon with sort-lowest when sort is asc' do
+        expect(project_sort_direction_button('created_asc')).to include('sort-lowest')
+      end
+
+      it 'returns icon with sort-lowest when sorting by milestone' do
+        expect(project_sort_direction_button('milestone')).to include('sort-lowest')
+      end
+
+      it 'returns icon with sort-lowest when sorting by due_date' do
+        expect(project_sort_direction_button('due_date')).to include('sort-lowest')
+      end
     end
 
-    it 'returns icon with sort-highest when sort is created_date' do
-      expect(project_sort_direction_button('created_date')).to include('sort-highest')
-    end
+    describe 'with project_list_filter_bar off' do
+      before do
+        stub_feature_flags(project_list_filter_bar: false)
+      end
 
-    it 'returns icon with sort-lowest when sort is asc' do
-      expect(project_sort_direction_button('created_asc')).to include('sort-lowest')
-    end
+      describe '#projects_sort_options_hash' do
+        it 'returns a hash of available sorting options' do
+          hash = old_projects_sort_options_hash
+          common_options = old_project_options
 
-    it 'returns icon with sort-lowest when sorting by milestone' do
-      expect(project_sort_direction_button('milestone')).to include('sort-lowest')
-    end
+          common_options.each do |key, opt|
+            expect(hash).to include(key)
+            expect(hash[key]).to eq(opt)
+          end
+        end
+      end
 
-    it 'returns icon with sort-lowest when sorting by due_date' do
-      expect(project_sort_direction_button('due_date')).to include('sort-lowest')
+      describe '#projects_reverse_sort_options_hash' do 
+        it 'returns a reversed hash of available sorting options' do
+          reverse_hash = projects_reverse_sort_options_hash
+
+          options = {
+            sort_value_latest_activity  => sort_value_oldest_activity,
+            sort_value_recently_created => sort_value_oldest_created,
+            sort_value_name             => sort_value_name_desc,
+            sort_value_stars_desc       => sort_value_stars_asc,
+            sort_value_oldest_activity  => sort_value_latest_activity,
+            sort_value_oldest_created   => sort_value_recently_created,
+            sort_value_name_desc        => sort_value_name,
+            sort_value_stars_asc        => sort_value_stars_desc
+          }
+
+          options.each do |key, opt|
+            expect(reverse_hash).to include(key)
+            expect(reverse_hash[key]).to eq(opt)
+          end
+        end
+      end
+
+      describe '#project_sort_direction_button' do
+        before do
+          allow(self).to receive(:request).and_return(double(path: 'http://test.com', query_parameters: { label_name: 'test_label' }))
+        end
+
+        it 'returns icon with sort-highest when sort is created_date' do
+          expect(project_sort_direction_button('created_date')).to include('sort-highest')
+        end
+
+        it 'returns icon with sort-lowest when sort is asc' do
+          expect(project_sort_direction_button('created_asc')).to include('sort-lowest')
+        end
+
+        it 'returns icon with sort-lowest when sorting by milestone' do
+          expect(project_sort_direction_button('milestone')).to include('sort-lowest')
+        end
+
+        it 'returns icon with sort-lowest when sorting by due_date' do
+          expect(project_sort_direction_button('due_date')).to include('sort-lowest')
+        end
+      end
     end
   end
 end
