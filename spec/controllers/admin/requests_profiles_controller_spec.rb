6219197c344f57d40b6cb3f0acfabcbc81a1ce28
Qingyu Zhao <qzhao@gitlab.com>
2019-07-08 22:39:44 +1000

    Update admin/requests_profiles_controller_spec
    Changes in this commit:
      - Add several spec helpers
      - Update 'load HTML report' example to use spec helpers
      - Add 'load TXT report' example
      - Add 'load PDF report' example

--- spec/controllers/admin/requests_profiles_controller_spec.rb
+++ spec/controllers/admin/requests_profiles_controller_spec.rb
@@ -10,38 +10,88 @@ describe Admin::RequestsProfilesController do
   end
 
   describe '#show' do
-    let(:basename) { "profile_#{Time.now.to_i}.html" }
-    let(:tmpdir) { Dir.mktmpdir('profiler-test') }
-    let(:test_file) { File.join(tmpdir, basename) }
-    let(:profile) { Gitlab::RequestProfiler::Profile.new(basename) }
-    let(:sample_data) do
-      <<~HTML
-        <!DOCTYPE html>
-        <html>
-        <body>
-        <h1>My First Heading</h1>
-        <p>My first paragraph.</p>
-        </body>
-        </html>
-      HTML
+    def generate_basename(profile_type, extension)
+      "profile_#{Time.now.to_i}_#{profile_type}.#{extension}"
     end
 
-    before do
-      stub_const('Gitlab::RequestProfiler::PROFILES_DIR', tmpdir)
-      output = File.open(test_file, 'w')
-      output.write(sample_data)
+    def prepare_profile_report_file(basename, sample_data)
+      tmpdir = Dir.mktmpdir('profiler-test')
+      test_file = File.join(tmpdir, basename)
+
+      create_file(tmpdir, test_file, sample_data)
+    end
+
+    def create_file(dir, file_path, data)
+      stub_const('Gitlab::RequestProfiler::PROFILES_DIR', dir)
+      output = File.open(file_path, 'w')
+      output.write(data)
       output.close
+
+      file_path
     end
 
-    after do
-      File.unlink(test_file)
+    def delete_file(file_path)
+      File.unlink(file_path)
     end
 
     it 'loads an HTML profile' do
+      sample_data =
+        <<~HTML
+          <!DOCTYPE html>
+          <html>
+          <body>
+          <h1>My First Heading</h1>
+          <p>My first paragraph.</p>
+          </body>
+          </html>
+        HTML
+
+      basename = generate_basename('execution', 'html')
+      test_file = prepare_profile_report_file(basename, sample_data)
+
       get :show, params: { name: basename }
 
       expect(response).to have_gitlab_http_status(200)
       expect(response.body).to eq(sample_data)
+
+      delete_file(test_file)
+    end
+
+    it 'loads a TXT profile' do
+      sample_data =
+        <<~TXT
+          Total allocated: 112096396 bytes (1080431 objects)
+          Total retained:  10312598 bytes (53567 objects)
+
+          allocated memory by gem
+          -----------------------------------
+              12416994  sprockets-3.7.2
+            11530224  json-1.8.6
+        TXT
+
+      basename = generate_basename('memory', 'txt')
+      test_file = prepare_profile_report_file(basename, sample_data)
+
+      get :show, params: { name: basename }
+
+      expect(response).to have_gitlab_http_status(200)
+      expect(response.body).to eq(sample_data)
+
+      delete_file(test_file)
+    end
+
+    it 'loads a PDF profile' do
+      sample_data =
+        <<~PDF
+          Sample content of a PDF file.
+        PDF
+
+      basename = generate_basename('memory', 'pdf')
+      test_file = prepare_profile_report_file(basename, sample_data)
+
+      expect { get :show, params: { name: basename } }.to raise_error(ActionController::UrlGenerationError, /No route matches.*unmatched constraints:/)
+
+      delete_file(test_file)
     end
   end
 end
