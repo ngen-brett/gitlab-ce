33c2a17025b0c99bfba82dd11fd2a16ca2759f9b
Ahmad Sherif <me@ahmadsherif.com>
2019-07-10 19:34:05 +0200

    Add caching to the archive endpoint

--- app/controllers/projects/repositories_controller.rb
+++ app/controllers/projects/repositories_controller.rb
@@ -23,10 +23,37 @@ class Projects::RepositoriesController < Projects::ApplicationController
       append_sha = false if @filename == shortname
     end
 
-    send_git_archive @repository, ref: @ref, path: params[:path], format: params[:format], append_sha: append_sha
+    kwargs = {ref: @ref, path: params[:path], format: params[:format], append_sha: append_sha}
+
+    return if cached_archive?(@repository, **kwargs)
+
+    send_git_archive @repository, **kwargs
   rescue => ex
     logger.error("#{self.class.name}: #{ex}")
     git_not_found!
+  end
+
+  private
+
+  def cached_archive?(repository, ref:, format:, append_sha:, path:)
+    storage_path = '' # Where archives are stored isn't really important for ETag purposes
+    metadata = repository.archive_metadata(ref, storage_path, format, append_sha: append_sha, path: path)
+    stale = stale?(etag: metadata['ArchivePath']) # The #stale? method sets cache headers.
+
+    # Because we are opinionated we set the cache headers ourselves.
+    response.cache_control[:public] = project.public?
+
+    response.cache_control[:max_age] =
+      if ref == metadata['CommitId']
+        # FIXME
+        Blob::CACHE_TIME_IMMUTABLE
+      else
+        # FIXME
+        Blob::CACHE_TIME
+      end
+
+    response.etag = metadata['ArchivePath']
+    !stale
   end
 
   def assign_archive_vars
