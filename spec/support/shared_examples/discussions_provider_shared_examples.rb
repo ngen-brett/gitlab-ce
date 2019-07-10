b0a844187bccda6b68a7c6b81577e4f0d3696389
Patrick Derichs <pderichs@gitlab.com>
2019-07-10 20:01:15 +0200

    Add schema validations for discussions

--- spec/support/shared_examples/discussions_provider_shared_examples.rb
+++ spec/support/shared_examples/discussions_provider_shared_examples.rb
@@ -7,6 +7,7 @@ shared_examples 'discussions provider' do
     get :discussions, params: { namespace_id: project.namespace, project_id: project, id: requested_iid }
 
     expect(response).to have_gitlab_http_status(200)
+    expect(response).to match_response_schema('entities/discussions')
 
     expect(json_response.size).to eq(expected_discussion_count)
     expect(json_response.pluck('id')).to eq(expected_discussion_ids)
