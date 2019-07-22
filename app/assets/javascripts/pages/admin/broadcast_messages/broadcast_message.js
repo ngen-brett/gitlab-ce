76f7f442fbdef875b25228f5662f98c5eb610e4c
Ammar Alakkad <aalakkad@gitlab.com>
2019-07-22 13:25:31 +0300

    Add default colors for broadcast messages

--- app/assets/javascripts/pages/admin/broadcast_messages/broadcast_message.js
+++ app/assets/javascripts/pages/admin/broadcast_messages/broadcast_message.js
@@ -4,7 +4,30 @@ import axios from '~/lib/utils/axios_utils';
 import flash from '~/flash';
 import { __ } from '~/locale';
 
+const hexToRgb = (hex) => {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const fullHex = hex.replace(shorthandRegex, (_m, r, g, b) => r + r + g + g + b + b);
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(fullHex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : null;
+}
+
+const textColorForBackground = (backgroundColor) => {
+  const [r, g, b] = hexToRgb(backgroundColor);
+
+  if (r + g + b > 500) {
+    return '#333333';
+  }
+  return '#FFFFFF';
+}
+
 export default () => {
+  const $broadcastMessageColor = $('input#broadcast_message_color');
   $('input#broadcast_message_color').on('input', function onMessageColorInput() {
     const previewColor = $(this).val();
     $('div.broadcast-message-preview').css('background-color', previewColor);
@@ -37,4 +60,27 @@ export default () => {
       }
     }, 250),
   );
+
+  const updateColorPreview = () => {
+    const selectedBackgroundColor = $('input#broadcast_message_color').val();
+    const contrastTextColor = textColorForBackground(selectedBackgroundColor);
+
+    // save contrastTextColor to hidden input field
+    $('input.text-font-color').val(contrastTextColor);
+
+    // Updates the preview color with the hex-color input
+    $('.label-color-preview').css('background-color', selectedBackgroundColor).css('color', contrastTextColor);
+    return $('div.broadcast-message-preview').css('background-color', selectedBackgroundColor).css('color', contrastTextColor);
+  }
+
+  const setSuggestedColor = (e) => {
+    const color = $(e.currentTarget).data('color');
+    $broadcastMessageColor.val(color)
+      // Notify the form, that color has changed
+      .trigger('input');
+    updateColorPreview();
+    return e.preventDefault();
+  };
+
+  $(document).on('click', '.suggest-colors a', setSuggestedColor);
 };
