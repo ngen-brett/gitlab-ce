e2f0ef3d3ba1780e3d918b708bb107df57fb3e0e
Luke Duncalfe <lduncalfe@eml.cc>
2019-07-24 13:53:06 +1200

    Redirect project.wiki.git to project wiki home
    https://gitlab.com/gitlab-org/gitlab-ce/issues/19186

---
+++ spec/routing/git_http_routing_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'git_http routing' do
+  include RSpec::Rails::RequestExampleGroup
+
+  describe 'wiki.git routing', 'routing' do
+    let(:wiki_path)  { '/gitlab/gitlabhq/wikis' }
+
+    it 'redirects namespace/project.wiki.git to the project wiki' do
+      expect(get('/gitlab/gitlabhq.wiki.git')).to redirect_to(wiki_path)
+    end
+
+    it 'only redirects when the format is .git' do
+      expect(get('/gitlab/gitlabhq.wiki')).not_to redirect_to(wiki_path)
+      expect(get('/gitlab/gitlabhq.wiki.json')).not_to redirect_to(wiki_path)
+    end
+  end
+end
