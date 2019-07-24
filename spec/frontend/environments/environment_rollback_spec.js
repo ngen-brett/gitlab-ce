f497514fb44280c570fcad4513f01145cf5a1e5d
Nathan Friend <nathan@gitlab.com>
2019-07-24 13:14:04 -0300

    Convert spec/javascripts/environments/*rollback* tests to Jest
    This commit converts two Jasmine tests into Jest tests.

--- spec/frontend/environments/environment_rollback_spec.js
+++ spec/frontend/environments/environment_rollback_spec.js
@@ -1,45 +1,43 @@
-import Vue from 'vue';
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
 import { GlButton } from '@gitlab/ui';
 import eventHub from '~/environments/event_hub';
 import rollbackComp from '~/environments/components/environment_rollback.vue';
 
 describe('Rollback Component', () => {
   const retryUrl = 'https://gitlab.com/retry';
+  const localVue = createLocalVue();
   let RollbackComponent;
 
   beforeEach(() => {
-    RollbackComponent = Vue.extend(rollbackComp);
+    RollbackComponent = localVue.extend(rollbackComp);
   });
 
   it('Should render Re-deploy label when isLastDeployment is true', () => {
-    const component = new RollbackComponent({
-      el: document.querySelector('.test-dom-element'),
+    const component = mount(RollbackComponent, {
       propsData: {
         retryUrl,
         isLastDeployment: true,
         environment: {},
       },
-    }).$mount();
+    });
 
-    expect(component.$el).toHaveSpriteIcon('repeat');
+    expect(component.element).toHaveSpriteIcon('repeat');
   });
 
   it('Should render Rollback label when isLastDeployment is false', () => {
-    const component = new RollbackComponent({
-      el: document.querySelector('.test-dom-element'),
+    const component = mount(RollbackComponent, {
       propsData: {
         retryUrl,
         isLastDeployment: false,
         environment: {},
       },
-    }).$mount();
+    });
 
-    expect(component.$el).toHaveSpriteIcon('redo');
+    expect(component.element).toHaveSpriteIcon('redo');
   });
 
   it('should emit a "rollback" event on button click', () => {
-    const eventHubSpy = spyOn(eventHub, '$emit');
+    const eventHubSpy = jest.spyOn(eventHub, '$emit');
     const component = shallowMount(RollbackComponent, {
       propsData: {
         retryUrl,
