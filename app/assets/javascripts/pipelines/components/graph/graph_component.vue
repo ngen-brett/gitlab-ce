d0e92c3dded8f7c8295f4dcecdbaebda439c6fc7
mfluharty <mfluharty@gitlab.com>
2019-07-25 17:21:03 -0600

    Extend pipeline graph scroll area to full width
    Use debounced resize function to set left padding inside graph
    Set padding such that the left edge of the pipeline lines up with
    the fixed-width container above it in the default scroll position

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -2,6 +2,9 @@
 import { GlLoadingIcon } from '@gitlab/ui';
 import StageColumnComponent from './stage_column_component.vue';
 import GraphMixin from '../../mixins/graph_component_mixin';
+import { debounceByAnimationFrame } from '~/lib/utils/common_utils';
+
+let debouncedResize;
 
 export default {
   components: {
@@ -9,11 +12,34 @@ export default {
     GlLoadingIcon,
   },
   mixins: [GraphMixin],
+  data() {
+    return {
+      leftPadding: 0,
+    };
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', debouncedResize);
+  },
+  created() {
+    this.onResize();
+    debouncedResize = debounceByAnimationFrame(this.onResize);
+    window.addEventListener('resize', debouncedResize);
+  },
+  methods: {
+    onResize(event) {
+      const width = event ? event.target.innerWidth : window.innerWidth;
+      if (width < 1330) this.leftPadding = 0;
+      else this.leftPadding = (width - 1330) / 2 + 70;
+    },
+  },
 };
 </script>
 <template>
   <div class="build-content middle-block js-pipeline-graph">
-    <div class="pipeline-visualization pipeline-graph pipeline-tab-content">
+    <div
+      class="pipeline-visualization pipeline-graph pipeline-tab-content"
+      :style="{ paddingLeft: `${leftPadding}px` }"
+    >
       <div v-if="isLoading" class="m-auto"><gl-loading-icon :size="3" /></div>
 
       <ul v-if="!isLoading" class="stage-column-list">
