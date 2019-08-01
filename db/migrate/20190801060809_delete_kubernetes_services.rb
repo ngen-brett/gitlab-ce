84c5eeb2631715c85e6c96ea8212d64b9792849d
Tiger <twatson@gitlab.com>
2019-08-01 16:20:35 +1000

    Final removal of KubernetesService
    Creating new records has been disabled, and all
    existing records been migrated to clusters as of
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/28534

---
+++ db/migrate/20190801060809_delete_kubernetes_services.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class DeleteKubernetesServices < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def up
+    Service.where(type: "KubernetesService").delete_all
+  end
+
+  def down
+  end
+end
