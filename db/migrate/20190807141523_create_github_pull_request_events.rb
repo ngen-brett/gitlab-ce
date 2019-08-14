8693ffba7460c2972097a076758196211117de4e
Fabio Pitino <fpitino@gitlab.com>
2019-08-14 10:57:46 +0200

    Port migration from EE

---
+++ db/migrate/20190807141523_create_github_pull_request_events.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateGithubPullRequestEvents < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def change
+    create_table :github_pull_request_events do |t|
+      t.timestamps_with_timezone null: false
+      t.references :project, null: false, foreign_key: { on_delete: :cascade }
+      t.string :branch_name, null: false, index: true
+      t.string :status, null: false, limit: 30
+
+      t.index [:project_id, :branch_name], unique: true
+    end
+  end
+end
