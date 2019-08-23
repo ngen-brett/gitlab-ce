b68a21d1d374caa577e921441151e41b7405859c
sstern <sstern@gitlab.com>
2019-08-23 14:35:22 -0700

    Add wip to handling update in issue description on polling

--- app/assets/javascripts/issue_show/stores/index.js
+++ app/assets/javascripts/issue_show/stores/index.js
@@ -1,8 +1,13 @@
 import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
 
+const DETAILS_OPEN = '<details open>';
+const DETAILS_EL_REGEXP = new RegExp(/<details>/, 'g');
+const DETAILS_TAG = '<details>';
+
 export default class Store {
   constructor(initialState) {
     this.state = initialState;
+    // maybe put details info here?
     this.formState = {
       title: '',
       description: '',
@@ -18,8 +23,30 @@ export default class Store {
     }
 
     Object.assign(this.state, convertObjectPropsToCamelCase(data));
+    // find if there is an open details node inside of the issue description.
+    const descriptionSection = $('details')
+    const atLeastOneDetailOpen = descriptionSection.find('details[open]').length > 0;
+  
+    if (atLeastOneDetailOpen) {
+      const details = descriptionSection.find('details');
+      let index = 0;
+
+      this.state.descriptionHtml = data.description.replace(DETAILS_EL_REGEXP, () => {
+        const shouldReplace = details[index] && details[index].open;
+        let str = DETAILS_TAG;
+
+        if(shouldReplace) {
+          str =  DETAILS_OPEN;
+        }
+        
+        index += 1;
+        return str;
+      });
+    } else {
+      this.state.descriptionHtml = data.description;
+    }
+    
     this.state.titleHtml = data.title;
-    this.state.descriptionHtml = data.description;
     this.state.lock_version = data.lock_version;
   }
 
