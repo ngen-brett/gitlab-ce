d658ca246aa8eb890d5d84d7fd8bef7edea724c7
sstern <sstern@gitlab.com>
2019-08-23 16:25:46 -0700

    Abstract updateDetailState into util

--- app/assets/javascripts/issue_show/stores/index.js
+++ app/assets/javascripts/issue_show/stores/index.js
@@ -1,8 +1,5 @@
 import { convertObjectPropsToCamelCase } from '~/lib/utils/common_utils';
-
-const DETAILS_OPEN = '<details open>';
-const DETAILS_EL_REGEXP = new RegExp(/<details>/, 'g');
-const DETAILS_TAG = '<details>';
+import { updateDetailsState } from '../utils/issue_description';
 
 export default class Store {
   constructor(initialState) {
@@ -28,19 +25,8 @@ export default class Store {
   
     if (atLeastOneDetailOpen) {
       const details = descriptionSection.find('details');
-      let index = 0;
 
-      this.state.descriptionHtml = data.description.replace(DETAILS_EL_REGEXP, () => {
-        const shouldReplace = details[index] && details[index].open;
-        let str = DETAILS_TAG;
-
-        if(shouldReplace) {
-          str =  DETAILS_OPEN;
-        }
-        
-        index += 1;
-        return str;
-      });
+      this.state.descriptionHtml = updateDetailsState(data.description, details);
     } else {
       this.state.descriptionHtml = data.description;
     }
