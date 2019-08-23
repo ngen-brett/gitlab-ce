41e1d966203190b735713ed69e5ee339af94ce33
Filipa Lacerda <filipa@gitlab.com>
2019-08-23 15:29:04 +0100

    Creates Feature Flag for job log

---
+++ app/assets/javascripts/jobs/components/job_log_json.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'JobLogJSON',
+};
+</script>
+<template>
+  <pre>
+    This feature is in development.
+    Please disable the `job_log_json` feature flag
+  </pre>
+</template>
