00c4e9294b47a597ca21b66a80996abc2e0861e6
Aishwarya Subramanian <asubramanian@gitlab.com>
2019-09-05 08:19:10 +0530

    Added init files for country gem

---
+++ config/initializers/countries.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+ISO3166.configure do |config|
+  config.locales = [:en]
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -111,6 +111,7 @@ Rails.application.routes.draw do
       draw :jira_connect
       draw :username
       draw :trial_registration
+      draw :country
     end
 
     Gitlab.ee do
