c8981718a608dc1116d82dad486e56059e1418ca
Steve Abrams <sabrams@gitlab.com>
2019-09-05 23:29:51 -0600

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_223900) do
+ActiveRecord::Schema.define(version: 2019_09_06_035623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
