33bd3ecf0c2cb70f0baf39b656d74d293132db9a
Patrick Derichs <pderichs@gitlab.com>
2019-09-06 10:31:12 +0200

    Split up SystemNoteService

---
+++ app/services/system_notes/base_system_note_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class BaseSystemNoteService
+  attr_reader :noteable, :project, :author
+
+  def initialize(noteable, project, author)
+    @noteable = noteable
+    @project = project
+    @author = author
+  end
+
+  def create_note(note_summary)
+    note = Note.create(note_summary.note.merge(system: true))
+    note.system_note_metadata = SystemNoteMetadata.new(note_summary.metadata) if note_summary.metadata?
+
+    note
+  end
+
+  def content_tag(*args)
+    ActionController::Base.helpers.content_tag(*args)
+  end
+
+  def url_helpers
+    @url_helpers ||= Gitlab::Routing.url_helpers
+  end
+
+  def cross_reference_note_prefix
+    'mentioned in '
+  end
+
+  def cross_reference?(note_text)
+    note_text =~ /\A#{cross_reference_note_prefix}/i
+  end
+
+end
