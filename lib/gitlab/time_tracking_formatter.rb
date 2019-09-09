02ac7f7c53ae9139979e12739eea53f3eab8c544
Aleksei Lipniagov <alipniagov@gitlab.com>
2019-09-09 15:34:30 +0300

    Pass :hours_per_day and :days_per_week to Chronic

--- lib/gitlab/time_tracking_formatter.rb
+++ lib/gitlab/time_tracking_formatter.rb
@@ -5,35 +5,18 @@ module Gitlab
     extend self
 
     def parse(string)
-      with_custom_config do
-        string = string.sub(/\A-/, '')
+      string = string.sub(/\A-/, '')
 
-        seconds = ChronicDuration.parse(string, default_unit: 'hours') rescue nil
-        seconds *= -1 if seconds && Regexp.last_match
-        seconds
-      end
+      seconds = ChronicDuration.parse(string, default_unit: 'hours', hours_per_day: 8, days_per_week: 5) rescue nil
+      seconds *= -1 if seconds && Regexp.last_match
+      seconds
     end
 
     def output(seconds)
-      with_custom_config do
-        ChronicDuration.output(seconds, format: :short, limit_to_hours: limit_to_hours_setting, weeks: true) rescue nil
-      end
+      ChronicDuration.output(seconds, format: :short, limit_to_hours: limit_to_hours_setting, weeks: true, hours_per_day: 8, days_per_week: 5) rescue nil
     end
 
     private
-
-    def with_custom_config
-      # We may want to configure it through project settings in a future version.
-      ChronicDuration.hours_per_day = 8
-      ChronicDuration.days_per_week = 5
-
-      result = yield
-
-      ChronicDuration.hours_per_day = 24
-      ChronicDuration.days_per_week = 7
-
-      result
-    end
 
     def limit_to_hours_setting
       Gitlab::CurrentSettings.time_tracking_limit_to_hours
