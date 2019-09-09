6702321d564adfbca741d1c1e2a21cd99171aaa6
Aleksei Lipniagov <alipniagov@gitlab.com>
2019-09-09 15:43:53 +0300

    Move custom config into const

--- lib/gitlab/time_tracking_formatter.rb
+++ lib/gitlab/time_tracking_formatter.rb
@@ -4,16 +4,19 @@ module Gitlab
   module TimeTrackingFormatter
     extend self
 
+    # We may want to configure it through project settings in a future version.
+    CUSTOM_DAY_AND_WEEK_LENGTH = { hours_per_day: 8, days_per_week: 5 }.freeze
+
     def parse(string)
       string = string.sub(/\A-/, '')
 
-      seconds = ChronicDuration.parse(string, default_unit: 'hours', hours_per_day: 8, days_per_week: 5) rescue nil
+      seconds = ChronicDuration.parse(string, default_unit: 'hours', **CUSTOM_DAY_AND_WEEK_LENGTH) rescue nil
       seconds *= -1 if seconds && Regexp.last_match
       seconds
     end
 
     def output(seconds)
-      ChronicDuration.output(seconds, format: :short, limit_to_hours: limit_to_hours_setting, weeks: true, hours_per_day: 8, days_per_week: 5) rescue nil
+      ChronicDuration.output(seconds, format: :short, limit_to_hours: limit_to_hours_setting, weeks: true, **CUSTOM_DAY_AND_WEEK_LENGTH) rescue nil
     end
 
     private
